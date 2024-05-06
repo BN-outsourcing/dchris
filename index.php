64160144d7a4b7f4c13ce89d02868ec6e3907071
Kim Ji Yu <spbabo97@naver.com>
2024-05-06 16:03:31 +0900

    :lipstick: UI 수정

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php 
     include_once $_SERVER["DOCUMENT_ROOT"]."/_head.php";
-    include_once $_SERVER["DOCUMENT_ROOT"]."/components/layout/_header.html";
 ?>
 
 <div id="smooth-wrapper">
     <div id="smooth-content">
         <main class="_main">
-            <section class="section01">
+            <section class="intro">
                 <video 
                     src="/asset/video/pc.mp4" 
                     autoplay 
@@ -15,119 +14,16 @@
                     loop
                 ></video>
 
-                <a href="/kor" class="kr select">
-                    KR
-                </a>
-                <a href="/eng" class="en select">
-                    en
-                </a>
+                <div class="sel-box">
+                    <a href="/kor" class="kr select">KR</a>
+                    <a href="/eng" class="en select">EN</a>
+                </div>
 
                 <p class="lang">
                     Welcome to DCHRIS<br/>
                     Please select a language.<br/>
                     KR or EN
                 </p>
-
-            </section>
-
-            <section class="section02">
-
-                <div class="img">
-                    <img class="img01" src="/asset/image/pc/1.jpg" alt="">
-                    <img class="img02" src="/asset/image/pc/2.jpg" alt="">
-                    <img class="img03" src="/asset/image/pc/3.jpg" alt="">
-                    <img class="img04" src="/asset/image/pc/4.jpg" alt="">
-                    <img class="img05" src="/asset/image/pc/5.jpg" alt="">
-                </div>
-
-                <div class="section02-one">
-
-                    <div class="flex">
-                        <div class="im-box">
-                            <div class="bb"></div>
-                            <div class="tbx">
-                                <p>DCHRIS</p>
-                                <div class="arr">-></div>
-                            </div>
-                        </div>
-    
-                        <div class="scroll-box">
-                            <p>SCROLL DOWN</p>
-                            <div class="scroll">
-                                <div class="cir"></div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="bbt">
-
-                        <div class="inline">
-                            <h2 class="bt">
-                                DCHRIS
-                            </h2>
-        
-                            <div class="p">
-                                <p>
-                                    D-black SMP Design is a professional. <br/>
-                                    As an SMP educational institution, the latest SMP <br/>
-                                    technology ASMP advanced technology developed <br/>
-                                    for the first time in the world based on the best <br/>
-                                    SMP expert leading formation.
-                                </p>
-                                <nav>
-                                    <a href="">ABOUT</a>
-                                    <a href="">MORE</a>
-                                </nav>
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div class="section02-two">
-
-                    <nav>
-                        <a href=""><i class="xi-whatsapp"></i> Whats app</a>
-                        <a href=""><i class="xi-speech"></i> KaKao Talk</a>
-                    </nav>
-
-                    <div class="col left-top">
-                        <p>D-BLACK ACADEMY</p>
-                        <div class="video"></div>
-                    </div>
-
-                    <div class="col left">
-                        <p>DCHRIS</p>
-                        <div class="video"></div>
-                    </div>
-
-                    <div class="col left">
-                        <p>DX BLACK</p>
-                        <div class="video"></div>
-                    </div>
-
-                    <div class="col left">
-                        <p>D-VERGENCE</p>
-                        <div class="video"></div>
-                    </div>
-
-                    <div class="col right">
-                        <p>D-BLACK PROJECT</p>
-                        <div class="video"></div>
-                    </div>
-
-                    <div class="col right">
-                        <p>D-SCALP CARE</p>
-                        <div class="video"></div>
-                    </div>
-
-                    <div class="col right">
-                        <p>D-STAR</p>
-                        <div class="video"></div>
-                    </div>
-
-                </div>
 
             </section>
         </main>
@@ -141,63 +37,6 @@
     ScrollSmoother.create({
         smooth: 1,
     });
-
-    ScrollTrigger.create({
-        trigger : "._main .section02 .img",
-        endTrigger : "._main .section02",
-        start : "top top",
-        pin : true,
-        pinSpace : true
-    });
-
-    $('._main .section02 .section02-two .col').mouseover(function(){
-
-        if($(this).hasClass('left-top')){
-
-            gsap.to('.img05',{
-                opacity : 1
-            });
-
-            gsap.to('.img01, .img02, .img04',{
-                opacity : 0
-            });
-
-        }
-
-        if($(this).hasClass('left')){
-
-            gsap.to('.img01',{
-                opacity : 1
-            });
-
-            gsap.to('.img02, .img04, .img05',{
-                opacity : 0
-            });
-
-        }
-
-        if($(this).hasClass('right')){
-
-            gsap.to('.img02',{
-                opacity : 1
-            });
-
-            gsap.to('.img01, .img04, .img05',{
-                opacity : 0
-            });
-
-        }
-
-    });
-
-    $('._main .section02 .section02-two .col').mouseleave(function(){
-
-        gsap.to('.img01, .img02, .img04, .img05',{
-            opacity : 0
-        });
-
-    });
-
 
 </script>
 
--- kor/index.php
+++ kor/index.php
@@ -3,41 +3,255 @@
     include_once $_SERVER["DOCUMENT_ROOT"]."/components/layout/_header.html";
 ?>
 
+<nav class="fix_nav">
+    <a href=""><i class="xi-whatsapp"></i> Whats app</a>
+    <a href=""><i class="xi-speech"></i> KaKao Talk</a>
+</nav>
+
+<div class="list-view">
+    <p class="scroll">SCROLL DOWN <i class="xi-angle-down-min"></i></p>
+    <section class="over">
+        <p class="small">
+            D-black SMP Design is a professional.<br/>
+            As an SMP educational institution, the latest SMP <br/>
+            technology ASMP advanced technology developed <br/>
+            for the first time in the world based on the best<br/>
+            SMP expert leading formation.
+        </p>
+        
+        <ul class="list">
+            <li>
+                <div class="img">
+                    <img src="/asset/image/main/list_view.jpg" alt="">
+                </div>
+                <div class="tbx">
+                    <p>① 디크리스</p>
+                    <a href="">VIEW</a>
+                </div>
+            </li>
+            <li>
+                <div class="img">
+                    <img src="/asset/image/main/list_view.jpg" alt="">
+                </div>
+                <div class="tbx">
+                    <p>① 디크리스</p>
+                    <a href="">VIEW</a>
+                </div>
+            </li>
+        </ul>
+    </section>
+</div>
+
 <div id="smooth-wrapper">
     <div id="smooth-content">
         <main class="_main">
-            <section class="section01">
-                <video 
-                    src="/asset/video/인트로 영상pc.mp4" 
-                    autoplay 
-                    muted 
-                    playsinline 
-                    loop
-                ></video>
 
-                <a href="/kor" class="kr select">
-                    KR
-                </p>
-                <a href="/eng" class="en select">
-                    en
-                </p>
+            <div class="desktop">
+                <div class="background-image">
+                    <img class="img01" src="/asset/image/pc/1.jpg" alt="">
+                    <img class="img02" src="/asset/image/pc/2.jpg" alt="">
+                    <img class="img03" src="/asset/image/pc/3.jpg" alt="">
+                    <img class="img04" src="/asset/image/pc/4.jpg" alt="">
+                    <img class="img05" src="/asset/image/pc/5.jpg" alt="">
+                </div>
 
-                <p class="lang">
-                    Welcome to DCHRIS<br/>
-                    Please select a language.<br/>
-                    KR or EN
-                </p>
+                <section class="section01">
 
-            </section>
-            <section class="section02">
-                
-            </section>
-            <section class="section03">
+                    <div class="im-box">
+                        <div class="bb"></div>
+                        <div class="tbx">
+                            <p>DCHRIS</p>
+                            <div class="line">
+                                <div class="arr"><img src="/asset/image/main/green-arr.png" alt=""></div>
+                            </div>
+                        </div>
+                    </div>
 
-            </section>
+                    <div class="scroll-box">
+                        <p>SCROLL DOWN</p>
+                        <div class="scroll">
+                            <div class="cir"></div>
+                        </div>
+                    </div>
+
+                    <div class="inline">
+                        <h2 class="bt">
+                            DCHRIS
+                        </h2>
+
+                        <div class="p">
+                            <p>
+                                D-black SMP Design is a professional. <br/>
+                                As an SMP educational institution, the latest SMP <br/>
+                                technology ASMP advanced technology developed <br/>
+                                for the first time in the world based on the best <br/>
+                                SMP expert leading formation.
+                            </p>
+                            <nav>
+                                <a href="">ABOUT</a>
+                                <a href="">MORE</a>
+                            </nav>
+                        </div>
+                    </div>
+
+                </section>
+
+                <section class="section02">
+
+                    <div class="col left-top">
+                        <p>D-BLACK ACADEMY</p>
+                        <div class="video">
+                            <iframe src="https://www.youtube.com/embed/zBXOZ_SvUqA?si=2xZJEVe_nU2dQuGu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </div>
+                    </div>
+
+                    <div class="col left">
+                        <p>DCHRIS</p>
+                        <div class="video">
+                            <iframe src="https://www.youtube.com/embed/zBXOZ_SvUqA?si=2xZJEVe_nU2dQuGu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </div>
+                    </div>
+
+                    <div class="col left">
+                        <p>DX BLACK</p>
+                        <div class="video">
+                            <iframe src="https://www.youtube.com/embed/zBXOZ_SvUqA?si=2xZJEVe_nU2dQuGu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </div>
+                    </div>
+
+                    <div class="col left">
+                        <p>D-VERGENCE</p>
+                        <div class="video">
+                            <iframe src="https://www.youtube.com/embed/zBXOZ_SvUqA?si=2xZJEVe_nU2dQuGu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </div>
+                    </div>
+
+                    <div class="col right">
+                        <p>D-BLACK PROJECT</p>
+                        <div class="video">
+                            <iframe src="https://www.youtube.com/embed/zBXOZ_SvUqA?si=2xZJEVe_nU2dQuGu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </div>
+                    </div>
+
+                    <div class="col right">
+                        <p>D-SCALP CARE</p>
+                        <div class="video">
+                            <iframe src="https://www.youtube.com/embed/zBXOZ_SvUqA?si=2xZJEVe_nU2dQuGu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </div>
+                    </div>
+
+                    <div class="col right">
+                        <p>D-STAR</p>
+                        <div class="video">
+                            <iframe src="https://www.youtube.com/embed/zBXOZ_SvUqA?si=2xZJEVe_nU2dQuGu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </div>
+                    </div>
+
+                </section>
+            </div>
+
+            <div class="mobile">
+
+                <div class="background-image">
+                    <img class="left" src="/asset/image/mob/mob1.jpg" alt="">
+                    <img class="center" src="/asset/image/mob/mob2.jpg" alt="">
+                    <img class="right" src="/asset/image/mob/mob3.jpg" alt="">
+                </div>
+
+                <nav class="m_nav">
+                    <a href=""><i class="xi-kakaotalk"></i> KaKao Talk</a>
+                    <a href=""><i class="xi-whatsapp"></i> Whats app</a>
+                </nav>
+
+            </div>
+
         </main>
     </div>
 </div>
+
+<script>
+
+    gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
+    
+    ScrollSmoother.create({
+        smooth: 1,
+    });
+
+    ScrollTrigger.create({
+        trigger : "._main .background-image",
+        endTrigger : "._main",
+        start : "top top",
+        pin : true,
+        pinSpace : true
+    });
+
+/* 
+
+    $('._main .section01 .section01-two .col').mouseover(function(){
+
+        if($(this).hasClass('left-top')){
+
+            gsap.to('.img05',{
+                opacity : 1
+            });
+
+            gsap.to('.img01, .img02, .img04',{
+                opacity : 0
+            });
+
+        }
+
+        if($(this).hasClass('left')){
+
+            gsap.to('.img01',{
+                opacity : 1
+            });
+
+            gsap.to('.img02, .img04, .img05',{
+                opacity : 0
+            });
+
+        }
+
+        if($(this).hasClass('right')){
+
+            gsap.to('.img02',{
+                opacity : 1
+            });
+
+            gsap.to('.img01, .img04, .img05',{
+                opacity : 0
+            });
+
+        }
+
+    });
+
+    $('._main .section01 .section01-two .col').mouseleave(function(){
+
+        gsap.to('.img01, .img02, .img04, .img05',{
+            opacity : 0
+        });
+
+    });
+
+    $('._main .section01 .section01-two .col').mouseover(function(){
+        gsap.to($(this).find('.video'),{
+            width : 377.99,
+            height : 213
+        });
+    });
+
+    $('._main .section01 .section01-two .col').mouseleave(function(){
+        gsap.to($(this).find('.video'),{
+            width : `${170*100/1920*1}vw`,
+            height : `${118*100/1920*1}vw`
+        });
+    });
+
+*/
+
+</script>
 
 
 <?php 
